9c0397b2f9ae7a402563e6f524a3fd95bf039ffc
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-15 01:17:24 -0300

    TouchableOpacity

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import {
   Platform,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
   StyleSheet,
   SafeAreaView,
@@ -13,7 +14,16 @@ export const Home = () => {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <Text style={styles.title}>Welcome, Dev!</Text>
-        <TextInput style={styles.input} />
+        <TextInput
+          placeholderTextColor="#555"
+          placeholder="Nova tarefa..."
+          style={styles.input}
+        />
+        <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.titleTasks}>Minhas Tarefas</Text>
       </View>
     </SafeAreaView>
   );
@@ -35,6 +45,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
+  titleTasks: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 50,
+  },
   input: {
     backgroundColor: '#29292e',
     color: '#f1f1f1',
@@ -42,5 +58,17 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 12,
     marginTop: 30,
     borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#eba417',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#121214',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
