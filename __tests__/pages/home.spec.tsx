e90b7283e75f2dbc29d64bacd9a49c5f94309928
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-20 02:08:43 -0300

    Home page test - parte 2

--- __tests__/pages/home.spec.tsx
+++ __tests__/pages/home.spec.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
+import {renderHook, act} from '@testing-library/react-hooks';
+
+import {TasksProvider, useTaskList} from '../../src/context/TasksContext';
 import {Home} from '../../src/pages/Home';
 
 describe('Home page', () => {
@@ -11,5 +14,17 @@ describe('Home page', () => {
     expect(inputNewTask).toBeDefined();
 
     expect(inputNewTask.props.placeholder).toBeTruthy();
+  });
+
+  it('verifica a insercao de um item na lista de tarefas', async () => {
+    const {result} = renderHook(() => useTaskList(), {
+      wrapper: TasksProvider,
+    });
+
+    const data = {id: 'Task01', title: 'Task01'};
+
+    await act(() => result.current.addTask(data));
+
+    expect(result.current.tasks).toBeTruthy();
   });
 });
