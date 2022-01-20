021d608f45b680cac119021e42d821beb9214270
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-20 02:47:03 -0300

    Jest custom config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "preset": "react-native",
+  "setupFilesAfterEnv": [
+    "@testing-library/jest-native/extend-expect"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules",
+    "/android",
+    "/ios"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ]
+}
