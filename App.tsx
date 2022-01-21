ea60f8850f18fc81e4b4dd9a2532abe2a213e00c
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-01-21 01:33:33 -0300

    Configurando a Splash Screen - parte 2

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {TasksProvider} from './src/context/TasksContext';
 import {Home} from './src/pages/Home';
 
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <TasksProvider>
       <Home />
--- android/app/src/main/java/com/mytasks/MainActivity.java
+++ android/app/src/main/java/com/mytasks/MainActivity.java
@@ -1,8 +1,15 @@
 package com.mytasks;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // Incluir esse import
+import android.os.Bundle; // Incluir esse import
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
